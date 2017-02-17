e59292e0a576786e2fae501114c1a8e2d716381c
Kenneth Geisshirt <kg@realm.io>
2017-02-17 12:29:43 +0100

    Use debug version

--- test/android/jni/Android.mk
+++ test/android/jni/Android.mk
@@ -4,7 +4,7 @@ include $(CLEAR_VARS)
 
 # Copy core to here - ARM only (see APP_ABI variable in Application.mk)
 LOCAL_MODULE     := realm-android
-LOCAL_SRC_FILES  := ../../../android-lib/librealm-android-arm.a
+LOCAL_SRC_FILES  := ../../../android-lib/librealm-android-arm-dbg.a
 include $(PREBUILT_STATIC_LIBRARY)
 
 include $(CLEAR_VARS)
@@ -15,8 +15,8 @@ TESTS := $(wildcard ../../test_*.cpp ../../large_tests/*.cpp ../../util/*.cpp ..
 LOCAL_MODULE     := native-activity
 LOCAL_SRC_FILES  := $(TESTS) main.cpp
 LOCAL_C_INCLUDES := $(LOCAL_PATH)/../../ ../../../src ../../../openssl/include/ # test, src, openssl
-LOCAL_CFLAGS     := -std=c++14 -DREALM_HAVE_CONFIG
-LOCAL_CPPFLAGS   := -std=c++14 -DREALM_HAVE_CONFIG
+LOCAL_CFLAGS     := -std=c++14 -DREALM_HAVE_CONFIG -g
+LOCAL_CPPFLAGS   := -std=c++14 -DREALM_HAVE_CONFIG -g
 LOCAL_LDLIBS     := -llog -landroid
 LOCAL_LDFLAGS    += -L../../android-lib
 LOCAL_STATIC_LIBRARIES += android_native_app_glue realm-android
